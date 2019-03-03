1cd43bc029552f91e810fad34833df1377f5fe46
thht95 <thht95@yahoo.com.vn>
2019-03-03 21:50:59 +0700

    1

--- QuanlyDontu/QuanlyDontu/Program.cs
+++ QuanlyDontu/QuanlyDontu/Program.cs
@@ -16,7 +16,7 @@ namespace QuanlyDontu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new Login());
         }
     }
 }
